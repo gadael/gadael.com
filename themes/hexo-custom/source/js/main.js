4770293a87b645b7a708b5a61c03d959fd312030
Paul de Rosanbo <paul@rosanbo.com>
2016-09-05 23:43:50 +0200

    CSS

--- themes/hexo-custom/source/js/main.js
+++ themes/hexo-custom/source/js/main.js
@@ -31,10 +31,10 @@
 
     // Profile card
     $(document).on('click', function () {
-        $('#profile').removeClass('card');
+        $('#create').removeClass('card');
     }).on('click', '#profile-anchor', function (e) {
         e.stopPropagation();
-        $('#profile').toggleClass('card');
+        $('#create').toggleClass('card');
     }).on('click', '.profile-inner', function (e) {
         e.stopPropagation();
     });
