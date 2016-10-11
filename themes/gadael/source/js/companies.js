2083066b841649c6afbf1d9fadc5c32a3a0ddfe7
Paul de Rosanbo <paul@rosanbo.com>
2016-10-11 09:15:42 +0200

    Fix two way binding

--- themes/gadael/source/js/companies.js
+++ themes/gadael/source/js/companies.js
@@ -7,8 +7,10 @@ angular.module('HexoCompanies', [])
 .controller("CompaniesController", ['$scope', '$http',
 function($scope, $http) {
 
-    $scope.dbname = 'hexo';
-    $scope.country = 'FR';
+    $scope.create = {
+        dbname: 'hexo',
+        country: 'FR'
+    };
 
     $http.get('/company').then(function(response) {
         $scope.company = response.data.company;
@@ -21,12 +23,7 @@ function($scope, $http) {
 
     $scope.createCompany = function() {
 
-        var create = {
-            dbname: $scope.dbname,
-            country: $scope.country
-        };
-
-        $http.post('/company', create).then(function(response) {
+        $http.post('/company', $scope.create).then(function(response) {
             console.log(response);
         }).catch(function(response) {
             var err = response.data;
