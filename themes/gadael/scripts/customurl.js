139b606d10fe6f6ada34073262b18e9ca005eee0
Paul de Rosanbo <paul@rosanbo.com>
2016-10-27 22:06:14 +0200

    Display available versions only

--- themes/gadael/scripts/customurl.js
+++ themes/gadael/scripts/customurl.js
@@ -2,7 +2,7 @@
 * Langurl Helper
 * @description Get the url from a path in the same language
 * @example
-*     <%- langurl('account/settings') %>
+*     <%= langurl('account/settings') %>
 */
 hexo.extend.helper.register('langurl', function(relativePath) {
     var currentRootFolder = this.page.path.split('/')[0];
@@ -11,16 +11,47 @@ hexo.extend.helper.register('langurl', function(relativePath) {
 
 
 
+
+
 /**
-* Versionurl Helper
-* Page with same file number and same language
-* @description Get the url from a documentation version folder name
+* getDocPageVersions Helper
+* @description Get page versions on same language
 * @example
-*     <%- versionurl('version-master') %>
+*     <% var arr = getDocPageVersions() %>
 */
-hexo.extend.helper.register('versionurl', function(folderName) {
-    var currentFolders = this.page.path.split('/');
-    var currentRootFolder = currentFolders[0];
-    var currentFile = currentFolders[3];
-    return '/'+currentRootFolder+'/docs/'+folderName+'/'+currentFile;
+hexo.extend.helper.register('getDocPageVersions', function(site) {
+    function pInfo(page) {
+        var currentFolders = page.path.split('/');
+
+        if (currentFolders.length !== 4 || currentFolders[1] !== 'docs') {
+            return null;
+        }
+
+        return {
+            lang: currentFolders[0],
+            version: currentFolders[2].substr('version-'.length),
+            num: currentFolders[3].substr(0, 3)
+        };
+    }
+
+    var here = pInfo(this.page);
+    var versions = [];
+
+    if (null === here) {
+        return [];
+    }
+
+    for (var i=0; i<site.pages.length; i++) {
+        var other = pInfo(site.pages.data[i]);
+        if (null !== other && other.lang === here.lang && other.num === here.num) {
+            versions.push({
+                url: '/'+site.pages.data[i].path,
+                label: other.version
+            });
+        }
+    }
+
+
+
+    return versions;
 });
